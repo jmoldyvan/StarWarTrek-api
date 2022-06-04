19f7d8327f9e23140e6108c105b5a23807ddcbde
jordan Moldovan <jmoldyvan@gmail.com>
2022-06-03 22:37:06 -0700

    cool

---
+++ main.js
@@ -0,0 +1,19 @@
+document.querySelector('#getButton').addEventListener('click', apiReq)
+
+async function apiReq(){
+    const alienName = document.querySelector('input').value
+    try {
+        const response = await fetch(`https://startrekwar-api.herokuapp.com/api/${alienName}`)
+        const data = await response.json()
+        console.log(data);
+        document.getElementById('alienName').innerText = data.speciesName
+        document.getElementById('alienWorld').innerText = data.homeWorld
+        document.getElementById('alienFeatures').innerText = data.features
+        document.getElementById('alienFacts').innerText = data.interestingFact
+        document.getElementById('alienImage').src = data.image
+        document.getElementById('alienCaption').innerText = data.speciesName
+    } catch (error){
+        console.log(error);
+    }
+
+}
